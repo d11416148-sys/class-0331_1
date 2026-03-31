59130b6292b2159bdf8f7251fdf0eb75a279c2c6
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-03-31 14:10:08 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public struct Student
+{
+    private int ID;
+    private string Name;
+    private Test Score;
+
+    public static int count;
+
+    public Student(int id, string n, int m, int f)
+    {
+        ID = id;
+        Name = n;
+        Score = new Test(m, f);
+
+        count++;
+    }
+
+    public string GetStudent()
+    {
+        string str;
+        str = " 學生編號: " + ID.ToString();
+        str += "\n 學生姓名: " + Name;
+        str += "\n 期中考成績: " + Score.GetMid().ToString();
+        str += "\n 期末考成績: " + Score.GetFinal().ToString();
+        str += "\n 平均成績: " + Score.GetAvg().ToString();
+        return str;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Class_0331_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student joe, mary;
+
+            joe = new Student(1234, "AAA", 92, 88);
+
+            Console.WriteLine(joe.GetStudent());
+
+            Console.WriteLine(Student.count.ToString());
+
+            //Console.WriteLine(joe.count.ToString());
+            mary = new Student(1234, "AAA", 92, 88);
+
+            //.WriteLine(mary.count.ToString());
+
+            Console.WriteLine(Student.count.ToString());
+        }
+    }
+}
